ad684f91ec18489c3a3e435961858676181000d2
Vinit <vk1501758@gmail.com>
2026-07-09 11:42:08 +0530

    fix: update user Schema pre-save hook to Promise-based style

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -47,15 +47,10 @@ const userSchema = new mongoose.Schema(
 );
 
 // Password hashing middleware
-userSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) return next();
-  try {
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-    next();
-  } catch (error) {
-    next(error);
-  }
+userSchema.pre('save', async function () {
+  if (!this.isModified('password')) return;
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
 });
 
 // Match password method
