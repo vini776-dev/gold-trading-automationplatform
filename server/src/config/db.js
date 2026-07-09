ffa67e19ecc07ea781ebd1ce0383f287f59b8b7b
Vinit <vk1501758@gmail.com>
2026-07-09 11:29:43 +0530

    fix: integrate Google DNS override in db.js configuration

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,4 +1,8 @@
 const mongoose = require('mongoose');
+const dns = require('dns');
+
+// Fix for Windows libuv DNS resolution bug with MongoDB Atlas SRV records
+dns.setServers(['8.8.8.8', '8.8.4.4']);
 
 const connectDB = async () => {
   try {
